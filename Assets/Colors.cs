2830b2548cfead970daa52899586f510483ff7a5
mikeriner <mikeriner@gmail.com>
2018-06-01 16:56:30 -0700

    Basic mostly empty project

---
+++ Assets/Colors.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colors : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+  float timer;
+	// Update is called once per frame
+	void Update () {
+    timer -= Time.deltaTime;
+    if (timer < 0)
+    {
+      GetComponent<MeshRenderer>().material.color = Random.ColorHSV();
+      timer = 3;
+    }
+	}
+}
---
+++ Assets/Rotater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour {
+
+  float rotation;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+   // var r = transform.rotation;
+    rotation += Time.deltaTime * 100;
+    transform.rotation = Quaternion.Euler(rotation, 0, -rotation);
+
+	}
+}
